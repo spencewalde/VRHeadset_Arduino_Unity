30cfce183c7e17260b14c2682c97c32f4f730092
spencewalde <waldemarspencer@gmail.com>
2020-08-26 15:52:01 -0500

    Update i2c_Uduino.ino

--- i2c_Uduino.ino
+++ i2c_Uduino.ino
@@ -1,38 +1,11 @@
 /*****************************************************************
-  LSM9DS1_Basic_I2C.ino
-  SFE_LSM9DS1 Library Simple Example Code - I2C Interface
-  Jim Lindblom @ SparkFun Electronics
-  Original Creation Date: April 30, 2015
-  https://github.com/sparkfun/LSM9DS1_Breakout
-
-  The LSM9DS1 is a versatile 9DOF sensor. It has a built-in
-  accelerometer, gyroscope, and magnetometer. Very cool! Plus it
-  functions over either SPI or I2C.
-
-  This Arduino sketch is a demo of the simple side of the
-  SFE_LSM9DS1 library. It'll demo the following:
-  How to create a LSM9DS1 object, using a constructor (global
-  variables section).
-  How to use the begin() function of the LSM9DS1 class.
-  How to read the gyroscope, accelerometer, and magnetometer
-  using the readGryo(), readAccel(), readMag() functions and
-  the gx, gy, gz, ax, ay, az, mx, my, and mz variables.
-  How to calculate actual acceleration, rotation speed,
-  magnetic field strength using the calcAccel(), calcGyro()
-  and calcMag() functions.
-  How to use the data from the LSM9DS1 to calculate
-  orientation and heading.
-
-  Hardware setup: This library supports communicating with the
-  LSM9DS1 over either I2C or SPI. This example demonstrates how
-  to use I2C. The pin-out is as follows:
+ LSM9DS1_Basic_I2C.ino library
+   The pin-out is as follows:
 	LSM9DS1 --------- Arduino
 	 SCL ---------- SCL (A5 on older 'Duinos')
 	 SDA ---------- SDA (A4 on older 'Duinos')
 	 VDD ------------- 3.3V
 	 GND ------------- GND
-  (CSG, CSXM, SDOG, and SDOXM should all be pulled high.
-  Jumpers on the breakout board will do this for you.)
 
   The LSM9DS1 has a maximum voltage of 3.6V. Make sure you power it
   off the 3.3V rail! I2C pins are open-drain, so you'll be
@@ -41,14 +14,9 @@
 
   Development environment specifics:
 	IDE: Arduino 1.6.3
-	Hardware Platform: SparkFun Redboard
+	Hardware Platform: arduino mega 2560
 	LSM9DS1 Breakout Version: 1.0
 
-  This code is beerware. If you see me (or any other SparkFun
-  employee) at the local, and you've found our code helpful,
-  please buy us a round!
-
-  Distributed as-is; no warranty is given.
 *****************************************************************/
 // The SFE_LSM9DS1 library requires both Wire and SPI be
 // included BEFORE including the 9DS1 library.
@@ -58,24 +26,16 @@
 #include <Uduino.h>
 #include <Servo.h>
 #define MAXSERVOS 8
+
 Uduino uduino("GYRO");
-//////////////////////////
 // LSM9DS1 Library Init //
-//////////////////////////
-// Use the LSM9DS1 class to create an object. [imu] can be
-// named anything, we'll refer to that throught the sketch.
+// Use the LSM9DS1 class to create an object. 
 LSM9DS1 imu;
 
-///////////////////////
-// Example I2C Setup //
-///////////////////////
 // SDO_XM and SDO_G are both pulled high, so our addresses are:
 // #define LSM9DS1_M	0x1E // Would be 0x1C if SDO_M is LOW
 // #define LSM9DS1_AG	0x6B // Would be 0x6A if SDO_AG is LOW
 
-////////////////////////////
-// Sketch Output Settings //
-////////////////////////////
 #define PRINT_CALCULATED
 //#define PRINT_RAW
 #define PRINT_SPEED 250 // 250 ms between prints
@@ -111,8 +71,8 @@ void setup()
     while (1);
   }
     
-  /////////////////////
-  #if defined (__AVR_ATmega32U4__) // Leonardo
+
+#if defined (__AVR_ATmega32U4__) // Leonardo
   while (!Serial) {}
 #elif defined(__PIC32MX__)
   delay(1000);
@@ -289,7 +249,7 @@ void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
 }
 
 
-/////////////////////////
+
 void ReadBundle() {
   char *arg = NULL;
   char *number = NULL;
@@ -487,3 +447,10 @@ void DisconnectAllServos() {
     servoPinMap[i] = -1;
   }
 }
+/*
+  LSM9DS1_Basic_I2C.ino
+  SFE_LSM9DS1 Library Simple Example Code - I2C Interface
+  Jim Lindblom @ SparkFun Electronics
+  Original Creation Date: April 30, 2015
+  https://github.com/sparkfun/LSM9DS1_Breakout
+*/
